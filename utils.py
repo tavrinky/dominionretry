5b7e4d39edcc0d10f7a7338e3f905b911a0be855
Eliza Brandt <jackbrandt11@gmail.com>
2021-04-26 07:51:58 -0700

    Added random string for player names

---
+++ utils.py
@@ -0,0 +1,6 @@
+import string 
+import random 
+def random_string(n): 
+    # https://stackoverflow.com/questions/2257441/random-string-generation-with-upper-case-letters-and-digits
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=n))
+    

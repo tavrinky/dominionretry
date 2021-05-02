baa7ca23cf989dc75cf8e5ec241b602c44eca17a
Eliza Brandt <jackbrandt11@gmail.com>
2021-05-02 01:22:46 -0700

    added triggered object in prep for merchant

--- utils.py
+++ utils.py
@@ -4,3 +4,9 @@ def random_string(n):
     # https://stackoverflow.com/questions/2257441/random-string-generation-with-upper-case-letters-and-digits
     return ''.join(random.choices(string.ascii_letters + string.digits, k=n))
     
+class Triggered: 
+    def __init__(self): 
+        self.triggered = False 
+
+    def trigger(self): 
+        self.triggered = True 
